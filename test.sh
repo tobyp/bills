22efc08e3a853effbffcd93a57a58a0ae5233c26
tobyp <tobyp@tobyp.net>
2019-11-20 00:23:16 +0100

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+shopt -s nullglob
+
+if [ -t 1 ]; then
+	RED="$(echo -ne '\033[7m\033[0;31m')"
+	GREEN="$(echo -ne '\033[7m\033[0;32m')"
+	RESET="$(echo -ne '\033[0m')"
+else
+	RED=''
+	GREEN=''
+	RESET=''
+fi
+
 result=0
-for test_input in $1/*.bills; do
+for test_input in "$1/"*".bills"; do
 	test_input_base="$(basename ${test_input})";
 	test_name="${test_input_base%.*}";
 	test_output="$1/${test_name}.output";
@@ -18,18 +30,32 @@ for test_input in $1/*.bills; do
 	fi
 
 	echo "Running test \"${test_name}\"...";
-	${test_cmd} 2>/dev/null | sort | diff -d - "${test_output}";
-	test_result=${PIPESTATUS[0]};
-	diff_result=$?;
+	if [ -e "${test_output}" ]; then
+		${test_cmd} 2>/dev/null | diff -d - "${test_output}";
+		test_result=${PIPESTATUS[0]};
+		diff_result=$?;
+	else
+		${test_cmd} 2>/dev/null
+		test_result=${PIPESTATUS[0]};
+		diff_result=0;
+	fi
+
 	if [ $test_result -ne $test_expected ]; then
-		echo " -> FAILED! (expected exit code ${test_expected}, got ${test_result})";
+		echo " -> ${RED}FAILED!${RESET} (expected exit code ${test_expected}, got ${test_result})";
 		result=1;
 	elif [ $diff_result -ne 0 ]; then
-		echo " -> FAILED! (output did not match what was expected)";
+		echo " -> ${RED}FAILED!${RESET} (output did not match what was expected)";
 		result=1;
 	else
-		echo " -> SUCCEEDED!"
+		echo " -> ${GREEN}PASSED!${RESET}"
 	fi
 done
+
+echo ""
+if [ $result != 0 ]; then
+	echo "${RED}Some tests failed!${RESET}"
+else
+	echo "${GREEN}All tests passed :)${RESET}"
+fi
 
 exit $result;
