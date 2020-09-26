37c89659f515a3ccf8b78792677c29f8385a5b41
tobyp <tobyp@tobyp.net>
2020-09-26 16:26:28 +0200

    Improve stdout and stderr output diffing in tests

--- test.sh
+++ test.sh
@@ -19,7 +19,8 @@ result=0
 for test_input in "${test_inputs[@]}"; do
 	test_input_base="$(basename ${test_input})";
 	test_name="${test_input_base%.*}";
-	test_output="$1/${test_name}.output";
+	test_exp_stdout="$1/${test_name}.stdout";
+	test_exp_stderr="$1/${test_name}.stderr";
 	test_cmd="./bills ${test_input}"
 
 	if [ -x "${test_input}" ]; then
@@ -32,27 +33,37 @@ for test_input in "${test_inputs[@]}"; do
 		test_expected=0
 	fi
 
+	test_run_stdout=$(mktemp ${test_name}-XXXXXX.stdout)
+	test_run_stderr=$(mktemp ${test_name}-XXXXXX.stderr)
 	echo "Running test \"${test_name}\"...";
-	if [ -e "${test_output}" ]; then
-		${test_cmd} 2>/dev/null | sort | diff -d - "${test_output}";
-		pipestatus=("${PIPESTATUS[@]}")
-		test_result=${pipestatus[0]};
-		diff_result=${pipestatus[2]};
-	else
-		${test_cmd} 2>/dev/null
-		pipestatus=("${PIPESTATUS[@]}")
-		test_result=${pipestatus[0]};
-		diff_result=0;
+	${test_cmd} >"${test_run_stdout}" 2>"${test_run_stderr}";
+	test_result=$?;
+	diff_result=0
+
+	if [ -e "${test_exp_stdout}" ]; then
+		sort "${test_run_stdout}" | diff -d - "${test_exp_stdout}";
+		diff_result=$(( $diff_result + ${PIPESTATUS[1]} ));
+	fi
+
+	if [ -e "${test_exp_stderr}" ]; then
+		diff -d "${test_run_stderr}" "${test_exp_stderr}";
+		diff_result=$(( $diff_result + $? ));
 	fi
 
 	if [ $test_result -ne $test_expected ]; then
 		echo " -> ${RED}FAILED!${RESET} (expected exit code ${test_expected}, got ${test_result})";
+		echo "   -- stdout: ${test_run_stdout}"
+		echo "   -- stderr: ${test_run_stderr}"
 		result=1;
 	elif [ $diff_result -ne 0 ]; then
 		echo " -> ${RED}FAILED!${RESET} (output did not match what was expected)";
+		echo "   -- stdout: ${test_run_stdout}"
+		echo "   -- stderr: ${test_run_stderr}"
 		result=1;
 	else
 		echo " -> ${GREEN}PASSED!${RESET}"
+		rm "${test_run_stdout}"
+		rm "${test_run_stderr}"
 	fi
 done
 
